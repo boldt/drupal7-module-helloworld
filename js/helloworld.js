b786160dd4a8a82cb3f79a40b8ab0fa78d70083d
Dennis Boldt <boldt@itm.uni-luebeck.de>
2015-06-23 11:16:47 +0200

    DELETE with AJAX

--- js/helloworld.js
+++ js/helloworld.js
@@ -2,22 +2,24 @@
     // We need a Drupal behavior, because DOM is not ready when this file is loaded
     Drupal.behaviors.hello = {
         attach: function (context, settings) {
-/*
-            var hello = $('#hello');
-            var colors = ['red', 'green', 'blue', 'yellow', 'orange'];
-            if(hello.length > 0) {
-                var p = $('<p>Hello World!</p>');
-                var button = $('<button class="btn">Change color</button>');
-                hello.append(p, button);
+            
+            // Handle delete
+            var helloworldDelete = $(".helloworld-delete");
+            helloworldDelete.each(function(index) {
+                var that = $(this);
+                that.click(function(){
+                    console.log("DELETE " + that.data("id"));
 
-                // This function updates the color of the `p`-element on each click
-                var i = 0;
-                button.click(function() {
-                    p.css('color', colors[i]);
-                    i = (i + 1) % colors.length;
+                    $.ajax({
+                        url: "drupal/?q=helloworld/rest/" + that.data("id"),
+                        type : "DELETE",
+                        success: function(result) {
+                            var e = $("#helloworld-context-" + that.data("id"));
+                            e.hide();
+                        }
+                    });
                 });
-            }
-*/
+            });
         }
     };
 
